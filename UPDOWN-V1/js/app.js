a450b4bf3b708874f5629637b37ee351ae8268b1
rlaban12 <rlaeogus9786@gmail.com>
2025-06-17 17:45:46 +0900

    20250617_1745 commit

--- UPDOWN-V1/js/app.js
+++ UPDOWN-V1/js/app.js
@@ -12,12 +12,18 @@ const correctNumber = Math.floor(Math.random() * 100) + 1;
 
 
 // 코드 실행 ======================================================================================================
-
+// 나의 도전기록 밑에 박스 나타나는지 테스트
+/*const upHistory = document.createElement('li');
+upHistory.textContent = "testtest";
+upHistory.classList.add('history-item');
+upHistory.classList.add('up');
+$historyList.append(upHistory);*/
 
 
 // 이벤트 바인딩 ======================================================================================================
 $guessBtn.addEventListener('click', e => {
 
+  e.preventDefault(); // 새로고침 막기
   // 정답 숫자 생성
 
 
@@ -29,16 +35,23 @@ $guessBtn.addEventListener('click', e => {
   // 입력한 값이 정답 -> "정답"
 
   if (inputNumber < correctNumber) {
-    // alert(`입력한 수는 ${inputNumber}, 업! 더 큰 수, 정답: ${correctNumber}`);
+    console.log(`입력한 수는 ${inputNumber}, 업! 더 큰 수, 정답: ${correctNumber}`);
     const upHistory = document.createElement('li');
+    upHistory.textContent = "up";
+    upHistory.classList.add('history-item');
     upHistory.classList.add('up');
     $historyList.append(upHistory);
 
   } else if (inputNumber > correctNumber) {
-    // alert(`입력한 수는 ${inputNumber}, 다운! 더 작은 수, 정답: ${correctNumber}`);
+    console.log(`입력한 수는 ${inputNumber}, 다운! 더 작은 수, 정답: ${correctNumber}`);
+    const upHistory = document.createElement('li');
+    upHistory.textContent = "down";
+    upHistory.classList.add('history-item');
+    upHistory.classList.add('down');
+    $historyList.append(upHistory);
 
   } else {
-    // alert(`입력한 수는 ${inputNumber}, 정답입니다!, 정답: ${correctNumber}`);
+    alert(`입력한 수는 ${inputNumber}, 정답입니다!, 정답: ${correctNumber}`);
 
   }
 
